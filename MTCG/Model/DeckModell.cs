a4a82daa0c0d47badfd81c08b8334154a3050f7b
Ngoc Lam Nguyen <if19b072@technikum-wien.at>
2020-10-12 11:02:01 +0200

    added functionality for temporarly adding cards to deck while in a game

--- MTCG/Model/DeckModell.cs
+++ MTCG/Model/DeckModell.cs
@@ -9,10 +9,12 @@ namespace MTCG.Model
     public class DeckModell
     {
         private readonly List<CardModell> _deckList;
+        private List<CardModell> _tempIngameCards;
 
         public DeckModell()
         {
             _deckList = new List<CardModell>();
+            _tempIngameCards = new List<CardModell>();
         }
         public bool Add(CardModell card)
         {
@@ -28,11 +30,49 @@ namespace MTCG.Model
                 return false;
             }
         }
+        
+        public bool AddTemp(CardModell card)
+        {
+            try
+            {
+                _deckList.Add(card);
+                _tempIngameCards.Add(card);
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
 
         public List<CardModell> GetDeck()
         {
             return _deckList;
         }
 
+        public bool Remove(CardModell card)
+        {
+            return _deckList.Remove(card);
+        }
+
+        public bool RemoveTempCards()
+        {
+            try
+            {
+                foreach (var card in _tempIngameCards)
+                {
+                    _deckList.Remove(card);
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
     }
 }
